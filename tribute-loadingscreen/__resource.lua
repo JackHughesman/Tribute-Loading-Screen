6624203da0cbc3a21b1a9c48e14590392a58102d
JackHughesman <51146542+JackHughesman@users.noreply.github.com>
2019-05-29 00:19:59 -0500

    Add files via upload

---
+++ tribute-loadingscreen/__resource.lua
@@ -0,0 +1,10 @@
+loadscreen 'index.html'
+files {
+    'index.html',
+    'css.css',
+    'audio.oga',
+}
+
+--This loading screen was made by J. Hughesman and developed for the intentional use of Humanity Roleplay (humanityroleplay.com)
+--You are authorized to use this. I request you not remove my watermark at the bottom, however I can't control what you do.
+--Please keep this note here.
